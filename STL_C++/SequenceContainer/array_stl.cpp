074ebcaa725cf37ce637421dea1e117fcbcf91d0
Mostafa-ashraf19 <mostaphaashraf1996@gmail.com>
2025-02-08 19:21:43 +0200

    STL array methods.

--- STL_C++/SequenceContainer/array_stl.cpp
+++ STL_C++/SequenceContainer/array_stl.cpp
@@ -2,8 +2,22 @@
 using namespace std;
 
 int main(int argc, char **argv)
-{   
+{
     std::array<int, 5> arr;
+    arr.fill(6);
+    std::cout << "arr.max_size()= " << arr.max_size() << '\n';
+
+    arr[4] = 10;
+    std::cout << "arr.front()= " << arr.front() << "arr.back()= " << arr.back() << '\n';
+
+    // Swap arr with arr2
+    // arr.swap(arr2)
+
+    std::cout << "boolalpha(arr.empty()) " << boolalpha << arr.empty() << '\n';
+
+    // Pointer to first element.
+    std::cout << "&arr[0] " << &arr[0] << '\n';
+    std::cout << "arr.data() " << arr.data() << '\n'; // &arr[0] equal to arr.data()
 
     std::cout << std::endl;
     return 0;
