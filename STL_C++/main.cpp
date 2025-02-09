f0ca3358c490dfe9e1444eecb6f2e5563b333454
Mostafa-ashraf19 <mostaphaashraf1996@gmail.com>
2025-02-09 07:42:44 +0200

    Initial commit Stack.

---
+++ STL_C++/main.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(int argc, char **argv)
+{
+
+    std::cout << std::endl;
+    return 0;
+}
---
+++ STL_C++/stack.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(int argc, char **argv)
+{
+
+    stack<int> s;
+    s.push(6);
+    s.push(1);
+    s.push(3);
+    s.push(5);
+    s.push(9);
+
+   
+
+    std::cout << std::endl;
+    return 0;
+}
